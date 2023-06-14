5de7c83dfb1a4a249788b0fbbf87100e92e32ec8
sebastienValla <sebastien.valla09@gmail.com>
2023-06-14 09:33:56 +0200

    add sopolia network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,26 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require('dotenv').config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/FfmH35zf4fifvH3eFrKPTSRi8IUW4aRV", // replace with your Sepolia RPC URL
+      accounts: [process.env.PK!], // replace with your account private key
+    }
+  },
+
+solidity: {
+    version: "0.8.18", // replace with your Solidity version
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
 
 export default config;

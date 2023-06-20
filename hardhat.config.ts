ba596c3f4ecb16abf1802c0d8f27916ec9095405
sebastienValla <sebastien.valla09@gmail.com>
2023-06-20 16:35:43 +0200

    add PK  validity check in config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,24 +1,34 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-require("dotenv").config();
-import "hardhat-gas-reporter";
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+require('dotenv').config()
+import 'hardhat-gas-reporter'
+
+const privateKey = process.env.PK!
+
+// Ethereum private key should be exactly 64 characters long (excluding `0x` prefix)
+// and should consist of only hexadecimal characters (0-9, a-f).
+const isPKValid = /^(0x)?[0-9a-fA-F]{64}$/.test(privateKey)
 
 const config: HardhatUserConfig = {
   gasReporter: {
-    currency: "USD",
+    currency: 'USD',
     gasPrice: 27,
   },
-  defaultNetwork: "hardhat",
+  defaultNetwork: 'hardhat',
   networks: {
     hardhat: {},
-    sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/FfmH35zf4fifvH3eFrKPTSRi8IUW4aRV", // replace with your Sepolia RPC URL
-      accounts: [process.env.PK!], // replace with your account private key
-    },
+    ...(isPKValid
+      ? {
+          sepolia: {
+            url: 'https://eth-sepolia.g.alchemy.com/v2/FfmH35zf4fifvH3eFrKPTSRi8IUW4aRV', // replace with your Sepolia RPC URL
+            accounts: [privateKey], // replace with your account private key
+          },
+        }
+      : {}),
   },
 
   solidity: {
-    version: "0.8.18", // replace with your Solidity version
+    version: '0.8.18', // replace with your Solidity version
     settings: {
       optimizer: {
         enabled: true,
@@ -26,6 +36,6 @@ const config: HardhatUserConfig = {
       },
     },
   },
-};
+}
 
-export default config;
+export default config

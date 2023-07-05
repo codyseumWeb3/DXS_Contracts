5dcc6bb54be155add8aac2ce0f013cbeaa1074d5
sebastienValla <sebastien.valla09@gmail.com>
2023-07-05 16:03:21 +0200

    add batch pay seller function and test to piggybank

--- contracts/PiggyBank.sol
+++ contracts/PiggyBank.sol
@@ -7,102 +7,188 @@ pragma solidity ^0.8.18;
 /// @notice This contract allows orders to be made, and allows the contract owner to pay the sellers while also distributing a percentage of the value to other addresses
 /// @dev This contract is not complete and should not be used in production
 contract PiggyBank {
-    address payable public daoAddress;
-    address payable public devAddress;
-    address payable public incentiveAddress;
-    address public owner;
-    uint public minProductPrice = 0.01 ether;
+  address payable public daoAddress;
+  address payable public devAddress;
+  address payable public incentiveAddress;
+  address public owner;
+  uint public minProductPrice = 0.01 ether;
 
-    mapping(address => uint) public pendingBalance;
+  mapping(address => uint) public pendingBalance;
 
-    event OrderMade(address indexed buyer, uint value);
-    event SellerPaid(address indexed seller, uint value);
-    event RefundGiven(address indexed buyer, uint value);
-    event MinProductPriceChanged(uint newMinProductPrice);
+  event OrderMade(address indexed buyer, uint value);
+  event SellerPaid(address indexed seller, uint value);
+  event RefundGiven(address indexed buyer, uint value);
+  event MinProductPriceChanged(uint newMinProductPrice);
 
-    /// @dev Constructor for the PiggyBank contract
-    /// @param _daoAddress the payable address for DAO
-    /// @param _devAddress the payable address for the Developer
-    /// @param _incentiveAddress the payable address for the incentive
-    constructor(address _daoAddress, address _devAddress, address _incentiveAddress) {
-        require(_daoAddress != address(0), 'DAO address cannot be the zero address.');
-        require(_devAddress != address(0), 'Developer address cannot be the zero address.');
-        require(_incentiveAddress != address(0), 'Incentive address cannot be the zero address.');
+  /// @dev Constructor for the PiggyBank contract
+  /// @param _daoAddress the payable address for DAO
+  /// @param _devAddress the payable address for the Developer
+  /// @param _incentiveAddress the payable address for the incentive
+  constructor(
+    address _daoAddress,
+    address _devAddress,
+    address _incentiveAddress
+  ) {
+    require(
+      _daoAddress != address(0),
+      'DAO address cannot be the zero address.'
+    );
+    require(
+      _devAddress != address(0),
+      'Developer address cannot be the zero address.'
+    );
+    require(
+      _incentiveAddress != address(0),
+      'Incentive address cannot be the zero address.'
+    );
 
-        owner = msg.sender;
-        daoAddress = payable(_daoAddress);
-        devAddress = payable(_devAddress);
-        incentiveAddress = payable(_incentiveAddress);
+    owner = msg.sender;
+    daoAddress = payable(_daoAddress);
+    devAddress = payable(_devAddress);
+    incentiveAddress = payable(_incentiveAddress);
+  }
+
+  /// @notice Allows a user to make an order by sending Ether to the contract
+  /// @dev The amount of Ether sent must be greater than the minimum product price
+  function makeOrder() external payable {
+    require(msg.value > minProductPrice, 'Value sent is too low.');
+    pendingBalance[msg.sender] += msg.value;
+    emit OrderMade(msg.sender, msg.value);
+  }
+
+  /// @notice Allows the contract owner to pay a seller and distribute the remaining value
+  /// @dev The value is distributed as follows: 2.5% to the DAO, 2% to the developers, 1% to the incentive, and the rest to the seller
+  /// @param sellerAddress the payable address of the seller
+  /// @param buyerAddress the address of the buyer
+  /// @param value the amount of Ether to be distributed
+  function paySeller(
+    address payable sellerAddress,
+    address buyerAddress,
+    uint value
+  ) external {
+    require(
+      sellerAddress != address(0),
+      'Seller address cannot be the zero address.'
+    );
+    require(msg.sender == owner, 'You are not the contract Owner.');
+    require(pendingBalance[buyerAddress] >= value, 'Not enough buyer balance.');
+    require(
+      address(this).balance >= value,
+      'Contract does not have enough balance.'
+    );
+
+    // Decrease the buyer's balance
+    pendingBalance[buyerAddress] -= value;
+
+    // Calculate the percentages
+    uint daoShare = (value * 25) / 1000; // 2.5%
+    uint devShare = (value * 2) / 100; // 2%
+    uint incentiveShare = (value * 1) / 100; //1%
+    uint sellerShare = value - daoShare - devShare - incentiveShare; // The rest goes to the seller
+
+    emit SellerPaid(sellerAddress, sellerShare);
+
+    // Transfer the values
+    daoAddress.transfer(daoShare);
+    devAddress.transfer(devShare);
+    incentiveAddress.transfer(incentiveShare);
+    sellerAddress.transfer(sellerShare);
+  }
+
+  /// @notice Allows the contract owner to pay multiple sellers and distribute the remaining value
+  /// @dev The value is distributed as follows: 2.5% to the DAO, 2% to the developers, 1% to the incentive, and the rest to the seller
+  /// @param sellersAddresses the payable addresses of the sellers
+  /// @param buyerAddresses the address of the buyer
+  /// @param values the amounts of Ether to be distributed to each seller
+  function batchPaySeller(
+    address payable[] memory sellersAddresses,
+    address[] memory buyerAddresses,
+    uint[] memory values
+  ) external {
+    require(
+      sellersAddresses.length == values.length,
+      'Sellers and values array length mismatch.'
+    );
+    require(
+      sellersAddresses.length == buyerAddresses.length,
+      'Sellers and Buyers array length mismatch.'
+    );
+    uint valueLeft;
+    for (uint i = 0; i < sellersAddresses.length; i++) {
+      address payable sellerAddress = sellersAddresses[i];
+      uint value = values[i];
+
+      require(
+        sellerAddress != address(0),
+        'Seller address cannot be the zero address.'
+      );
+      require(msg.sender == owner, 'You are not the contract Owner.');
+      require(
+        pendingBalance[buyerAddresses[i]] >= value,
+        'Not enough buyer balance for this seller.'
+      );
+      require(
+        address(this).balance >= value,
+        'Contract does not have enough balance.'
+      );
+
+      // Decrease the buyer's balance
+      pendingBalance[buyerAddresses[i]] -= value;
+
+      uint sellerShare = (value * 945) / 1000; // The seller received 94.5% (100% - 5.5% fees
+
+      valueLeft += value - sellerShare;
+      emit SellerPaid(sellerAddress, sellerShare);
+
+      sellerAddress.transfer(sellerShare);
     }
 
-    /// @notice Allows a user to make an order by sending Ether to the contract
-    /// @dev The amount of Ether sent must be greater than the minimum product price
-    function makeOrder() external payable {
-        require(msg.value > minProductPrice, 'Value sent is too low.');
-        pendingBalance[msg.sender] += msg.value;
-        emit OrderMade(msg.sender, msg.value);
-    }
+    // Calculate the percentages
+    uint daoShare = (valueLeft * 4545) / 10000; // 45.45% of total remaining value (2.5% of the original transaction value)
+    uint incentiveShare = (valueLeft * 1818) / 10000; // 18.18 % of total remaining value (1% of the original transaction value)
+    uint devShare = valueLeft - daoShare - incentiveShare; // 2%
 
-    /// @notice Allows the contract owner to pay a seller and distribute the remaining value
-    /// @dev The value is distributed as follows: 2.5% to the DAO, 2% to the developers, 1% to the incentive, and the rest to the seller
-    /// @param sellerAddress the payable address of the seller
-    /// @param buyerAddress the address of the buyer
-    /// @param value the amount of Ether to be distributed
-    function paySeller(address payable sellerAddress, address buyerAddress, uint value) external {
-        require(sellerAddress != address(0), 'Seller address cannot be the zero address.');
-        require(msg.sender == owner, 'You are not the contract Owner.');
-        require(pendingBalance[buyerAddress] >= value, 'Not enough buyer balance.');
-        require(address(this).balance >= value, 'Contract does not have enough balance.');
+    require(
+      address(this).balance >= (daoShare + devShare + incentiveShare),
+      'Contract does not have enough balance for DAO, dev and incentive shares.'
+    );
+    // Transfer the values
+    daoAddress.transfer(daoShare);
+    devAddress.transfer(devShare);
+    incentiveAddress.transfer(incentiveShare);
+  }
 
-        // Decrease the buyer's balance
-        pendingBalance[buyerAddress] -= value;
+  /// @notice Allows the contract owner to refund a buyer
+  /// @dev The value specified must be less than or equal to the buyer's balance
+  /// @param buyerAddress the payable address of the buyer
+  /// @param value the amount of Ether to be refunded
+  function refund(address payable buyerAddress, uint value) external {
+    require(msg.sender == owner, 'You are not the contract Owner.');
+    require(pendingBalance[buyerAddress] >= value, 'Not enough buyer balance.');
+    pendingBalance[buyerAddress] -= value;
+    emit RefundGiven(buyerAddress, value);
+    buyerAddress.transfer(value);
+  }
 
-        // Calculate the percentages
-        uint daoShare = (value * 25) / 1000; // 2.5%
-        uint devShare = (value * 2) / 100; // 2%
-        uint incentiveShare = (value * 1) / 100; //1%
-        uint sellerShare = value - daoShare - devShare - incentiveShare; // The rest goes to the seller
+  /// @notice Allows the contract owner to set the minimum product price
+  /// @dev The minimum product price is set in wei
+  /// @param newMinProductPrice the new minimum product price
+  function setMinProductPrice(uint newMinProductPrice) external {
+    require(msg.sender == owner, 'You are not the contract Owner.');
+    minProductPrice = newMinProductPrice;
+    emit MinProductPriceChanged(newMinProductPrice);
+  }
 
-        emit SellerPaid(sellerAddress, sellerShare);
+  /// @notice Allows the current owner to transfer control of the contract to a new owner
+  /// @param newOwner The address to transfer ownership to
+  function transferOwnership(address newOwner) external {
+    require(msg.sender == owner, 'You are not the contract Owner.');
+    require(newOwner != address(0), 'New owner cannot be the zero address.');
+    owner = newOwner;
+  }
 
-        // Transfer the values
-        daoAddress.transfer(daoShare);
-        devAddress.transfer(devShare);
-        incentiveAddress.transfer(incentiveShare);
-        sellerAddress.transfer(sellerShare);
-    }
-
-    /// @notice Allows the contract owner to refund a buyer
-    /// @dev The value specified must be less than or equal to the buyer's balance
-    /// @param buyerAddress the payable address of the buyer
-    /// @param value the amount of Ether to be refunded
-    function refund(address payable buyerAddress, uint value) external {
-        require(msg.sender == owner, 'You are not the contract Owner.');
-        require(pendingBalance[buyerAddress] >= value, 'Not enough buyer balance.');
-        pendingBalance[buyerAddress] -= value;
-        emit RefundGiven(buyerAddress, value);
-        buyerAddress.transfer(value);
-    }
-
-    /// @notice Allows the contract owner to set the minimum product price
-    /// @dev The minimum product price is set in wei
-    /// @param newMinProductPrice the new minimum product price
-    function setMinProductPrice(uint newMinProductPrice) external {
-        require(msg.sender == owner, 'You are not the contract Owner.');
-        minProductPrice = newMinProductPrice;
-        emit MinProductPriceChanged(newMinProductPrice);
-    }
-
-    /// @notice Allows the current owner to transfer control of the contract to a new owner
-    /// @param newOwner The address to transfer ownership to
-    function transferOwnership(address newOwner) external {
-        require(msg.sender == owner, 'You are not the contract Owner.');
-        require(newOwner != address(0), 'New owner cannot be the zero address.');
-        owner = newOwner;
-    }
-
-    /// @dev Fallback function to reject any Ether sent directly to the contract
-    fallback() external {
-        revert('Do not send Ether directly.');
-    }
+  /// @dev Fallback function to reject any Ether sent directly to the contract
+  fallback() external {
+    revert('Do not send Ether directly.');
+  }
 }
--- test/PiggyBank.ts
+++ test/PiggyBank.ts
@@ -1,127 +1,205 @@
-import { loadFixture } from '@nomicfoundation/hardhat-network-helpers'
-import { expect } from 'chai'
-import { ethers } from 'hardhat'
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
 
 describe('PiggyBank Contract Tests', function () {
   async function deployPiggyBank() {
-    const [owner, dev, dao, incentive, user1, user2] = await ethers.getSigners()
-    const PiggyBankFactory = await ethers.getContractFactory('PiggyBank')
-    const piggyBankInstance = await PiggyBankFactory.deploy(dao.address, dev.address, incentive.address)
+    const [owner, dev, dao, incentive, user1, user2, user3] =
+      await ethers.getSigners();
+    const PiggyBankFactory = await ethers.getContractFactory('PiggyBank');
+    const piggyBankInstance = await PiggyBankFactory.deploy(
+      dao.address,
+      dev.address,
+      incentive.address
+    );
 
-    return { piggyBankInstance, owner, user1, user2 }
+    return { piggyBankInstance, owner, user1, user2, user3 };
   }
 
   describe('Deployment Tests', function () {
     it('Should deploy the contract and set the owner correctly', async function () {
-      const { piggyBankInstance, owner } = await loadFixture(deployPiggyBank)
+      const { piggyBankInstance, owner } = await loadFixture(deployPiggyBank);
 
-      expect(await piggyBankInstance.owner()).to.equal(owner.address)
-    })
-  })
+      expect(await piggyBankInstance.owner()).to.equal(owner.address);
+    });
+  });
 
   describe('Deposit Tests', function () {
     it('Should allow users to deposit Ether', async function () {
-      const { piggyBankInstance, user1 } = await loadFixture(deployPiggyBank)
+      const { piggyBankInstance, user1 } = await loadFixture(deployPiggyBank);
 
-      const depositAmount = ethers.utils.parseEther('1')
-      const initialBalance = await piggyBankInstance.pendingBalance(user1.address)
+      const depositAmount = ethers.utils.parseEther('1');
+      const initialBalance = await piggyBankInstance.pendingBalance(
+        user1.address
+      );
 
-      await piggyBankInstance.connect(user1).makeOrder({ value: depositAmount })
+      await piggyBankInstance
+        .connect(user1)
+        .makeOrder({ value: depositAmount });
 
-      expect(await piggyBankInstance.pendingBalance(user1.address)).to.equal(initialBalance.add(depositAmount))
-    })
-  })
+      expect(await piggyBankInstance.pendingBalance(user1.address)).to.equal(
+        initialBalance.add(depositAmount)
+      );
+    });
+  });
 
   describe('Seller Payment Tests', function () {
     it('Should allow owner to pay sellers', async function () {
-      const { piggyBankInstance, owner, user1 } = await loadFixture(deployPiggyBank)
-      const paymentAmount = ethers.utils.parseEther('1')
+      const { piggyBankInstance, owner, user1 } = await loadFixture(
+        deployPiggyBank
+      );
+      const paymentAmount = ethers.utils.parseEther('1');
 
-      await piggyBankInstance.connect(user1).makeOrder({ value: paymentAmount })
+      await piggyBankInstance
+        .connect(user1)
+        .makeOrder({ value: paymentAmount });
 
       // Pay the seller and check the contract balance and pendingBalance
-      await piggyBankInstance.connect(owner).paySeller(user1.address, user1.address, paymentAmount)
-      expect(await piggyBankInstance.pendingBalance(user1.address)).to.equal(0)
-    })
-  })
+      await piggyBankInstance
+        .connect(owner)
+        .paySeller(user1.address, user1.address, paymentAmount);
+      expect(await piggyBankInstance.pendingBalance(user1.address)).to.equal(0);
+    });
+  });
+
+  describe('Seller Batch Payment Tests', function () {
+    it('Should allow owner to pay several sellers in one transaction', async function () {
+      const { piggyBankInstance, owner, user1, user2, user3 } =
+        await loadFixture(deployPiggyBank);
+      const paymentAmount = ethers.utils.parseEther('1');
+      const paymentAmount2 = ethers.utils.parseEther('2');
+      const paymentAmount3 = ethers.utils.parseEther('3');
+
+      // Make orders
+      await piggyBankInstance
+        .connect(user1)
+        .makeOrder({ value: paymentAmount });
+      await piggyBankInstance
+        .connect(user2)
+        .makeOrder({ value: paymentAmount2 });
+      await piggyBankInstance
+        .connect(user3)
+        .makeOrder({ value: paymentAmount3 });
+
+      // Pay the sellers and check the contract balance and pendingBalance
+      await piggyBankInstance
+        .connect(owner)
+        .batchPaySeller(
+          [user1.address, user2.address, user3.address],
+          [user1.address, user2.address, user3.address],
+          [paymentAmount, paymentAmount2, paymentAmount3]
+        );
+
+      // Validate the balances
+      expect(await piggyBankInstance.pendingBalance(user1.address)).to.equal(0);
+      expect(await piggyBankInstance.pendingBalance(user2.address)).to.equal(0);
+      expect(await piggyBankInstance.pendingBalance(user3.address)).to.equal(0);
+    });
+  });
 
   describe('Refund Tests', function () {
     it('Should allow owner to refund buyers', async function () {
-      const { piggyBankInstance, owner, user1 } = await loadFixture(deployPiggyBank)
-      const depositAmount = ethers.utils.parseEther('1')
+      const { piggyBankInstance, owner, user1 } = await loadFixture(
+        deployPiggyBank
+      );
+      const depositAmount = ethers.utils.parseEther('1');
 
-      await piggyBankInstance.connect(user1).makeOrder({ value: depositAmount })
+      await piggyBankInstance
+        .connect(user1)
+        .makeOrder({ value: depositAmount });
 
-      await piggyBankInstance.connect(owner).refund(user1.address, depositAmount)
-      expect(await piggyBankInstance.pendingBalance(user1.address)).to.equal(0)
-    })
-  })
+      await piggyBankInstance
+        .connect(owner)
+        .refund(user1.address, depositAmount);
+      expect(await piggyBankInstance.pendingBalance(user1.address)).to.equal(0);
+    });
+  });
 
   describe('Ownership Transfer Tests', function () {
     it('Should allow owner to transfer ownership', async function () {
-      const { piggyBankInstance, owner, user2 } = await loadFixture(deployPiggyBank)
+      const { piggyBankInstance, owner, user2 } = await loadFixture(
+        deployPiggyBank
+      );
 
-      await piggyBankInstance.connect(owner).transferOwnership(user2.address)
-      expect(await piggyBankInstance.owner()).to.equal(user2.address)
-    })
-  })
+      await piggyBankInstance.connect(owner).transferOwnership(user2.address);
+      expect(await piggyBankInstance.owner()).to.equal(user2.address);
+    });
+  });
 
   describe('Change Product Price Tests', function () {
     it('Should allow owner to change minimum product price', async function () {
-      const { piggyBankInstance, owner } = await loadFixture(deployPiggyBank)
-      const newMinProductPrice = ethers.utils.parseEther('0.02')
+      const { piggyBankInstance, owner } = await loadFixture(deployPiggyBank);
+      const newMinProductPrice = ethers.utils.parseEther('0.02');
 
-      await piggyBankInstance.connect(owner).setMinProductPrice(newMinProductPrice)
-      expect(await piggyBankInstance.minProductPrice()).to.equal(newMinProductPrice)
-    })
-  })
+      await piggyBankInstance
+        .connect(owner)
+        .setMinProductPrice(newMinProductPrice);
+      expect(await piggyBankInstance.minProductPrice()).to.equal(
+        newMinProductPrice
+      );
+    });
+  });
 
   describe('Failure Tests', function () {
     it('Should not allow users to deposit below the minimum product price', async function () {
-      const { piggyBankInstance, user1 } = await loadFixture(deployPiggyBank)
+      const { piggyBankInstance, user1 } = await loadFixture(deployPiggyBank);
 
       await expect(
-        piggyBankInstance.connect(user1).makeOrder({ value: ethers.utils.parseEther('0.005') })
-      ).to.be.revertedWith('Value sent is too low.')
-    })
+        piggyBankInstance
+          .connect(user1)
+          .makeOrder({ value: ethers.utils.parseEther('0.005') })
+      ).to.be.revertedWith('Value sent is too low.');
+    });
 
     it('Should not allow non-owners to pay sellers', async function () {
-      const { piggyBankInstance, user1, user2 } = await loadFixture(deployPiggyBank)
-      const depositAmount = ethers.utils.parseEther('1')
+      const { piggyBankInstance, user1, user2 } = await loadFixture(
+        deployPiggyBank
+      );
+      const depositAmount = ethers.utils.parseEther('1');
 
-      await piggyBankInstance.connect(user1).makeOrder({ value: depositAmount })
+      await piggyBankInstance
+        .connect(user1)
+        .makeOrder({ value: depositAmount });
 
       await expect(
-        piggyBankInstance.connect(user2).paySeller(user1.address, user1.address, depositAmount)
-      ).to.be.revertedWith('You are not the contract Owner.')
-    })
+        piggyBankInstance
+          .connect(user2)
+          .paySeller(user1.address, user1.address, depositAmount)
+      ).to.be.revertedWith('You are not the contract Owner.');
+    });
 
     it('Should not allow non-owners to refund buyers', async function () {
-      const { piggyBankInstance, user1, user2 } = await loadFixture(deployPiggyBank)
-      const depositAmount = ethers.utils.parseEther('1')
+      const { piggyBankInstance, user1, user2 } = await loadFixture(
+        deployPiggyBank
+      );
+      const depositAmount = ethers.utils.parseEther('1');
 
-      await piggyBankInstance.connect(user1).makeOrder({ value: depositAmount })
+      await piggyBankInstance
+        .connect(user1)
+        .makeOrder({ value: depositAmount });
 
-      await expect(piggyBankInstance.connect(user2).refund(user1.address, depositAmount)).to.be.revertedWith(
-        'You are not the contract Owner.'
-      )
-    })
+      await expect(
+        piggyBankInstance.connect(user2).refund(user1.address, depositAmount)
+      ).to.be.revertedWith('You are not the contract Owner.');
+    });
 
     it('Should not allow non-owners to transfer ownership', async function () {
-      const { piggyBankInstance, user1, user2 } = await loadFixture(deployPiggyBank)
+      const { piggyBankInstance, user1, user2 } = await loadFixture(
+        deployPiggyBank
+      );
 
-      await expect(piggyBankInstance.connect(user1).transferOwnership(user2.address)).to.be.revertedWith(
-        'You are not the contract Owner.'
-      )
-    })
+      await expect(
+        piggyBankInstance.connect(user1).transferOwnership(user2.address)
+      ).to.be.revertedWith('You are not the contract Owner.');
+    });
 
     it('Should not allow non-owners to change minimum product price', async function () {
-      const { piggyBankInstance, user1 } = await loadFixture(deployPiggyBank)
-      const newMinProductPrice = ethers.utils.parseEther('0.02')
+      const { piggyBankInstance, user1 } = await loadFixture(deployPiggyBank);
+      const newMinProductPrice = ethers.utils.parseEther('0.02');
 
-      await expect(piggyBankInstance.connect(user1).setMinProductPrice(newMinProductPrice)).to.be.revertedWith(
-        'You are not the contract Owner.'
-      )
-    })
-  })
-})
+      await expect(
+        piggyBankInstance.connect(user1).setMinProductPrice(newMinProductPrice)
+      ).to.be.revertedWith('You are not the contract Owner.');
+    });
+  });
+});

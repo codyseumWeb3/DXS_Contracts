927d235f8f2310e53889f49b8875943c4bc33786
Fabco <contact@fabco.tech>
2023-08-18 10:17:51 +0200

    Added unique deploy script for each contract, uniformized CamelCase for each contract name

---
+++ scripts/deployPiggyBank.ts
@@ -0,0 +1,25 @@
+import { ethers } from 'hardhat';
+
+async function main() {
+  const devWalletAddress = '0x684653D5315f1Cb36cE13814F42C7f83F1791168';
+  const daoWalletAddress = '0x91C4594072Fc6D62D7Af2772c0E9309E8F380d72';
+  const arrbitratorWalletAddress = '0x91C4594072Fc6D62D7Af2772c0E9309E8F380d72';
+
+  const PiggyBank = await ethers.getContractFactory('PiggyBank');
+  const contract = await PiggyBank.deploy(
+    daoWalletAddress,
+    devWalletAddress,
+    arrbitratorWalletAddress
+  );
+
+  await contract.deployed();
+
+  console.log(`PiggyBank contract deployed to ${contract.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});

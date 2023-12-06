e3b43635bac67b21529e299ca45c6092d0bb2ace
sebastienValla <sebastien.valla09@gmail.com>
2023-12-06 13:58:31 +0100

    add bach script

---
+++ publish-npm-dev.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+npm version --preid dev --no-git-tag-version --no-commit-hooks prepatch
+#Use timestamp as package suffix
+TIME=$(date -u +%Y%m%d%H%M%S)
+sed -i "/version/s/dev.0/dev.$TIME/g" package.json
+PUBLISH_VERSION=$(cat package.json | jq -r '.version')
+echo publishing codyseumWeb3/contracts/$PUBLISH_VERSION
+
+npm publish --access public --tag dev

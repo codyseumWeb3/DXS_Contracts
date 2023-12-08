34080df5dca9519b713578ae2547c978bcbef7bc
sebastienValla <sebastien.valla09@gmail.com>
2023-12-08 16:06:49 +0100

    deploy script

--- scripts/addresses.ts
+++ scripts/addresses.ts
@@ -1,4 +1,5 @@
 export const addresses = {
   dxsWalletAddress: '0xd466F93b3C9D687e866669762fB07b9d3E3f2709',
   supplierWalletAddress: '0x6e1CF93438082b29dea00371c820240ddce4A918',
+  MoneyReceiverReceiver : '0xd466F93b3C9D687e866669762fB07b9d3E3f2709',
 };

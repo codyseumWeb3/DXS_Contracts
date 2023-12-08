5c2cf0740503472848fc31a3dcadd46c4c652d76
sebastienValla <sebastien.valla09@gmail.com>
2023-12-08 09:07:27 +0100

    add etherscan api key to hardhatconfig

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,9 +4,8 @@ require('dotenv').config()
 import 'hardhat-gas-reporter'
 
 const privateKey = process.env.PK!
+const etherscanApiKey = process.env.ETHERSCAN_API_KEY! // Ensure this is set in your .env file
 
-// Ethereum private key should be exactly 64 characters long (excluding `0x` prefix)
-// and should consist of only hexadecimal characters (0-9, a-f).
 const isPKValid = /^(0x)?[0-9a-fA-F]{64}$/.test(privateKey)
 
 const config: HardhatUserConfig = {
@@ -20,21 +19,23 @@ const config: HardhatUserConfig = {
     ...(isPKValid
       ? {
           sepolia: {
-            url: 'https://eth-sepolia.g.alchemy.com/v2/FfmH35zf4fifvH3eFrKPTSRi8IUW4aRV', // replace with your Sepolia RPC URL
-            accounts: [privateKey], // replace with your account private key
+            url: 'https://eth-sepolia.g.alchemy.com/v2/FfmH35zf4fifvH3eFrKPTSRi8IUW4aRV',
+            accounts: [privateKey],
           },
         }
       : {}),
   },
-
   solidity: {
-    version: '0.8.18', // replace with your Solidity version
+    version: '0.8.18',
     settings: {
       optimizer: {
         enabled: true,
         runs: 200,
       },
     },
+  },
+  etherscan: {
+    apiKey: etherscanApiKey,
   },
 }
 

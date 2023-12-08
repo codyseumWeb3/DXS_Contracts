213cfbc3935ad11ac9f2e4bfa41d200a39ffcfa8
sebastienValla <sebastien.valla09@gmail.com>
2023-12-08 15:16:21 +0100

    verify contract on etherscan on deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,12 @@
-import { HardhatUserConfig } from 'hardhat/config'
-import '@nomicfoundation/hardhat-toolbox'
-require('dotenv').config()
-import 'hardhat-gas-reporter'
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+require('dotenv').config();
+import 'hardhat-gas-reporter';
 
-const privateKey = process.env.PK!
-const etherscanApiKey = process.env.ETHERSCAN_API_KEY! // Ensure this is set in your .env file
+const privateKey = process.env.PK!;
+const etherscanApiKey = process.env.ETHERSCAN_API_KEY!; // Ensure this is set in your .env file
 
-const isPKValid = /^(0x)?[0-9a-fA-F]{64}$/.test(privateKey)
+const isPKValid = /^(0x)?[0-9a-fA-F]{64}$/.test(privateKey);
 
 const config: HardhatUserConfig = {
   gasReporter: {
@@ -35,8 +35,11 @@ const config: HardhatUserConfig = {
     },
   },
   etherscan: {
-    apiKey: etherscanApiKey,
+    apiKey: {
+      mainnet: etherscanApiKey,
+      sepolia: etherscanApiKey,
+    },
   },
-}
+};
 
-export default config
+export default config;
--- scripts/deployMarketPlaceFees.ts
+++ scripts/deployMarketPlaceFees.ts
@@ -1,21 +1,56 @@
-import { ethers } from 'hardhat';
+import { ethers, run } from 'hardhat';
 import { addresses } from './addresses';
 
 async function main() {
+  console.log("Initializing contract factory...");
   const MarketPlaceFees = await ethers.getContractFactory('MarketPlaceFees');
+  console.log("Contract factory initialized.");
+
+  console.log("Deploying contract...");
   const contract = await MarketPlaceFees.deploy(
     addresses.dxsWalletAddress,
     addresses.supplierWalletAddress
   );
 
+  console.log("Awaiting contract deployment...");
   await contract.deployed();
-
   console.log(`MarketPlaceFees contract deployed to ${contract.address}`);
+
+  if (process.env.VERIFY === 'true') {
+    console.log("Starting verification process. Environment variable VERIFY is set to true.");
+
+    console.log("Waiting 60 seconds before starting verification...");
+    await delay(60000); // Delay for 60 seconds
+
+    for (let i = 0; i < 3; i++) { // Retry up to 3 times
+      try {
+        console.log(`Verification attempt ${i + 1}...`);
+        await run('verify:verify', {
+          address: contract.address,
+          constructorArguments: [
+            addresses.dxsWalletAddress,
+            addresses.supplierWalletAddress,
+          ],
+        });
+        console.log('Contract verified on Etherscan');
+        break; // Break the loop if verification is successful
+      } catch (error) {
+        console.error(`Attempt ${i + 1} failed to verify contract on Etherscan:`, error);
+        if (i < 2) {
+          console.log(`Waiting 30 seconds before retrying attempt ${i + 2}...`);
+          await delay(30000); // Wait for 30 seconds before retrying
+        }
+      }
+    }
+  } else {
+    console.log("Skipping contract verification. Environment variable VERIFY is not set to true.");
+  }
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+  console.error("Deployment script encountered an error:", error);
 });
